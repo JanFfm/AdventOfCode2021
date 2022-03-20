1a0446261380f514a4d564ef3e86e9c93a349795
JanFfm <45403124+JanFfm@users.noreply.github.com>
2022-03-20 21:37:36 +0100

    2b

--- Day2b.py
+++ Day2b.py
@@ -1,28 +1,32 @@
 
+
 class submarine:
     horizontal_position = 0
     depth = 0
+    aim = 0
 
     def up(self, x):
-         self.depth -=x
+        self.aim -=x
     def down(self, x):
-         self.depth +=x
+        self.aim +=x
    
     def forward(self, x):
          self.horizontal_position +=x
+         self.depth += x * self.aim
     
     def get_input(self, s: str):
         comand = s.split(' ')[0]
         value = int(s.split(' ')[1])
         if comand == "up":
             self.up(value)
-        if comand == "down":
+        elif comand == "down":
             self.down(value)
-        if comand == "forward":
+        elif comand == "forward":
             self.forward(value)
 
 
 sub =submarine()
+
 input_file = open('input2a.txt', 'r')
 for l in input_file:
     sub.get_input(l.split('\n')[0])
